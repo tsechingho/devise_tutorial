3ca2ab37eba80c5ece2d51bec52c1c8ba476b0f7
Tse-Ching Ho <tsechingho@gmail.com>
2011-07-16 16:47:38 +0800

    rails new devise_tutorial -JTd mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseTutorial::Application.config.secret_token = '8d03f6dd4ea8c84b78191c2a78d2697976d8fcc532bae53e8ca25e082ebaa0a6d81ec333ded6b26e59b2ce07120943abfeff84eceb0c756463a4395e12043307'
