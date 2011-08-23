7d38e8276bbb313a31081988061b8c70c892635d
Tse-Ching Ho <tsechingho@gmail.com>
2011-08-23 15:19:30 +0800

    modify books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,20 @@
 class Book < ActiveRecord::Base
-  belongs_to :author
+  validates :url, :title, :author_id, :presence => true
+
+  belongs_to :author, :class_name => "User"
+
+  has_attached_file :cover_image,
+    :style => {
+      :thumb => "160x120#"
+    }
+  validates_attachment_content_type :cover_image,
+    :content_type => [ "image/jpeg", "image/png", "image/gif", "image/pjpeg", "image/x-png" ]
+  validates_attachment_size :cover_image,
+    :less_than => 2.megabytes
+
+  acts_as_url :title
+
+  def to_param
+    url
+  end
 end
