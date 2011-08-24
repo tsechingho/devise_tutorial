8624dc952e9e0dad940ed1221564c9dcaed6b720
Tse-Ching Ho <tsechingho@gmail.com>
2011-08-24 17:36:36 +0800

    rails g model authentication resource:references provider:string uid:string uname:string uemail:string

---
+++ db/migrate/20110824093525_create_authentications.rb
@@ -0,0 +1,17 @@
+class CreateAuthentications < ActiveRecord::Migration
+  def self.up
+    create_table :authentications do |t|
+      t.references :resource
+      t.string :provider
+      t.string :uid
+      t.string :uname
+      t.string :uemail
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :authentications
+  end
+end
